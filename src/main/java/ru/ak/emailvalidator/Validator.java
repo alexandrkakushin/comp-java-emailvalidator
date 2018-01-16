c0ed44bdc6ca8580af0683a102048a3d9d940ba0
Alexander Kakushin <alexandrkakushin@yandex.ru>
2018-01-16 15:02:06 +0300

    Добавление проверки на обязательное наличие домена второго уровня

--- src/main/java/ru/ak/emailvalidator/Validator.java
+++ src/main/java/ru/ak/emailvalidator/Validator.java
@@ -5,6 +5,7 @@ import org.hibernate.validator.internal.constraintvalidators.hv.EmailValidator;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
+import java.util.regex.Pattern;
 
 /**
  * Web-сервис для проверки валидности адреса электронной почты
@@ -21,7 +22,14 @@ public class Validator {
      */
     @WebMethod
     public boolean isValid(@WebParam(name = "email") String email) {
-          EmailValidator validator = new EmailValidator();
-          return validator.isValid(email, null);
+        boolean isValid = false;
+
+        EmailValidator validator = new EmailValidator();
+        Pattern ptr = Pattern.compile("^[-\\w.]+@([A-z0-9][-A-z0-9]+\\.)+[A-z]{2,4}$");
+
+        isValid = validator.isValid(email, null);
+        isValid = isValid && ptr.matcher(email).matches();
+
+        return isValid;
     }
 }
